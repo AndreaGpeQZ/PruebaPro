89629bbd7755fe3fc1d37f0077d5409965dabc5e
AndreaGpeQZ <andi.agq@gmail.com>
2024-10-30 17:25:10 +0000

    :v

--- DireccionEntrega/backup.sh
+++ DireccionEntrega/backup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-BACKUP_FILE="/backups/backup_$TIMESTAMP.tar.gz"
 
-# Comprime el directorio /var/www/html en el contenedor
-tar -czvf "$BACKUP_FILE" /var/www/html
+# Variables
+REPO_URL="https://github.com/AndreaGpeQZ/PruebaPro.git"
+BACKUP_DIR="/backups/$(date +'%Y-%m-%d_%H-%M-%S')"
+
+# Crea el directorio de backup
+mkdir -p "$BACKUP_DIR"
+
+# Clona o actualiza el repositorio en el directorio de backups
+if [ -d "$BACKUP_DIR/.git" ]; then
+  cd "$BACKUP_DIR"
+  git pull
+else
+  git clone "$REPO_URL" "$BACKUP_DIR"
+fi
